2379e6621d6b7be0e494ab31f877f4ac1aaa8a7c
박지현 <byeoring@byeoring.local>
2024-06-30 01:42:18 +0900

    first commit

---
+++ src/bean/mmpm1002/MMPM1002A01.java
@@ -0,0 +1,13 @@
+package bean.mmpm1002;
+
+import inf.EA_SMYP1002A001_DBSMCD9999Input;
+import inf.EA_SMYP1002A001_DBSMCD9999Output;
+
+public class MMPM1002A01 {
+    public EA_SMYP1002A001_DBSMCD9999Output process(EA_SMYP1002A001_DBSMCD9999Input input) {
+        // 서비스 로직 작성
+        EA_SMYP1002A001_DBSMCD9999Output output = new EA_SMYP1002A001_DBSMCD9999Output();
+        output.setDate(input.getDate());
+        return output;
+    }
+}
